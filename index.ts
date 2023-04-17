00afd5aaaec78345df5528e65b43fde72845d7df
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2023-04-17 15:42:12 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+import express, {Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { DataSource } from 'typeorm';
+const app: Express = express();
+dotenv.config();
+app.use(bodyParser.json());
+app.use(cors());
+const port  = process.env.PORT
+//Create Database Connection
+export const AppDataSource = new DataSource( {
+   type: 'mysql',
+   host: process.env.HOST,
+   port: 3306,
+   username: process.env.MYSQL_USER,
+   password: process.env.MYSQL_PASSWORD,
+   database: process.env.MYSQL_DB,
+   synchronize: true,
+});
+
+
+app.get('/', (req: Request, res: Response) => {
+   res.send("Express + Typescript Server");
+})
+
+AppDataSource.initialize()
+   .then(() => {
+   app.listen(port);
+   console.log("Database connection established!");
+})
+   .catch((err)=> {
+   console.error( 'Error during DataSource initialization', err)
+   })
+
+
