ea200eead9ae06572ff0152d977d1e2a198c784f
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2023-04-18 12:32:03 +0100

    creating post endpoint

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,29 @@
-import { Task } from "./tasks.entity";
+import { AppDataSource } from '../../index';
+import { Task } from './tasks.entity';
+import { instanceToPlain } from 'class-transformer';
 
 export class TasksController {
-   public async getAll(): Promise<Task[]> {}
-} 
+   constructor(
+      private taskRepository = AppDataSource.getRepository(
+         Task,
+      ),
+   ) {}
+   public async getAll(): Promise<Task[]>{
+      //Decalre a variable to hold all tasks
+      let allTasks: Task[];
+      //Fetch all tasks using the repository
+      try{
+         allTasks = await this.taskRepository.find({
+         order: {
+            date: 'ASC',
+         },
+      });
+      //Convert the tasks instance to an array of  objects
+      allTasks = instanceToPlain(allTasks) as Task[];
+      return allTasks;
+      }catch(errors){
+         console.log('Message: ', errors);
+         throw errors;
+      }
+   }
+}
--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,8 +1,14 @@
 import { Router, Request, Response } from "express";
+import { TasksController } from "./tasks.controller";
 
 //Fire the router functiom
 export const tasksRouter: Router = Router();
 //Create a default route
-tasksRouter.get('/tasks', (req: Request, res: Response) => {
-   res.send('Express + Typescript Server');
+tasksRouter.get('/tasks', async (req: Request, res: Response) => {
+   const taskController = new TasksController();
+   const allTasks = await taskController.getAll();
+   res.status(200).json(allTasks);
+});
+tasksRouter.post('/tasks', async (req:Request, res: Response) => {
+   const taskController = new TasksController();
 });
