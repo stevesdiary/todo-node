1e530468409ac6ca23c53025dbfce37feee7696c
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2023-04-20 08:28:09 +0100

    Using the validator

--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import { TasksController } from "./tasks.controller";
-
+import { createValidator } from "./tasks.validator";
+import { validationResult } from "express-validator";
 //Fire the router functiom
 export const tasksRouter: Router = Router();
 //Create a default route
@@ -9,6 +10,12 @@ tasksRouter.get('/tasks', async (req: Request, res: Response) => {
    const allTasks = await taskController.getAll();
    res.status(200).json(allTasks);
 });
-tasksRouter.post('/tasks', async (req:Request, res: Response) => {
-   const taskController = new TasksController();
+
+tasksRouter.post('/tasks', createValidator, async (req:Request, res: Response) => {
+   const errors = validationResult(req);
+   // const taskController = new TasksController();
+
+   if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array()});
+   }
 });
--- src/tasks/tasks.validator.ts
+++ src/tasks/tasks.validator.ts
@@ -1,4 +1,29 @@
-import { body } from 'express-validator'; 
-export const createValidator = [
-   
+import { ValidationChain, body } from 'express-validator'; 
+import { Priority } from '../enums/Priority';
+import { Status } from '../enums/Status';
+export const createValidator: ValidationChain[] = [
+   body('title')
+      .not()
+      .isEmpty()
+      .withMessage('The task title is mandatory')
+      .trim()
+      .isString()
+      .withMessage('The task title needs to be a string'),
+   body('date')
+      .not()
+      .isEmpty()
+      .withMessage('Date is required')
+      .trim().isString()
+      .withMessage('Date needs to be a valid date format'),
+   body('description')
+      .trim()
+      .isString()
+      .withMessage('Description needs to be in text format'),
+   body('priority')
+      .trim()
+      .isIn([Priority.normal, Priority.high, Priority.low])
+      .withMessage('Priority needs to be normal, high or low.'),
+   body('status')
+      .trim()
+      .isIn([Status.todo, Status.inProgress, Status.completed])
 ];
